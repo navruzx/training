1370a9cb7322352a0bd094c225f48312108005ac
navruzx <nnavruz095@gmail.com>
2024-08-19 17:42:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector(".btn");
+let menu = document.querySelector(".menu");
+let box = document.querySelector(".box");
+let box1 = document.querySelector(".box1");
+function openKategory() {
+  menu.classList.toggle("menu-block");
+}
